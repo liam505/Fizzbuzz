c1167e1ad00231a7c151ff56b0190f0befbca3dd
Liam <monteroliam@gmail.com>
2020-03-18 12:47:05 +0000

    add functionality

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,1 +1,18 @@
 // Fizzbuzz function
+function fizzbuzz(n){
+    for(let i = 0; i <= n; i++){
+        if(i % 15 === 0){
+            console.log("fizzbuz")
+        }
+        else if(i % 5 === 0){
+            console.log("fizz")
+        }
+        else if(i % 3 === 0){
+            console.log("buzz")
+        }
+        else{
+            console.log(i)
+        }
+    }
+}
+fizzbuzz(20)
